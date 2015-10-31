97ba7ba77fa55f98cfc83bc737b2f83fb9e0e55a
Noel OConnor <noel.oconnor@gmail.com>
2015-10-31 11:09:43 +1100

    changed health check to use 8080

--- sensor1.js
+++ sensor1.js
@@ -16,7 +16,7 @@ app.get('/', function (req, res) {
     res.sendStatus(processing_status);
 });
 
-var server = app.listen(3000, function () {
+var server = app.listen(8080, function () {
     var host = server.address().address;
     var port = server.address().port;
 
